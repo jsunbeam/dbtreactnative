34a2ea5b614efa6a2f09475e7b9c2dad7e5fb556
Jessica Shao <jshaodev@gmail.com>
2023-09-23 22:49:48 -0500

    Tab Navigation and HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,19 @@
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-elements";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
   },
   image: {
-    flex: 1,
     margin: 0,
-    padding: 0,
-    height: 20,
+    padding: 50,
+    height: 200,
     width: 200,
   },
+  button: {},
 });
 const HomeScreen = () => {
   return (
@@ -22,9 +22,14 @@ const HomeScreen = () => {
         source={require("../assets/images/panda-avatar.png")}
         style={styles.image}
       />
-      <Button color="secondary" title="Emotions" />
-      <Button title="Urges" />
-      <Button title="SOS" />
+      <Text>DBT Panda</Text>
+      <Text>
+        Don't know which DBT skills to use? I can help! Choose what you're
+        feeling below to get started.
+      </Text>
+      <Button color="secondary" title="Emotions" style={styles.button} />
+      <Button title="Urges" style={styles.button} />
+      <Button title="SOS" style={styles.button} />
     </View>
   );
 };
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,17 +1,37 @@
 import { View, Text } from "react-native";
+import { Icon } from "react-native-elements";
 import HomeScreen from "./HomeScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const screenOptions = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#5637DD" },
+};
+
+const HomeNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          title: "Home",
+          headerLeft: () => <Icon name="home" type="font-awesome" />,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const Main = () => {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        padding: 50,
-      }}
-    >
-      <Text>Testing random text</Text>
-      <HomeScreen />
-    </View>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomeNavigator} />
+    </Tab.Navigator>
   );
 };
 
